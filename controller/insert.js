af7983af17e9509c51a93b9490fae18517f11ff3
Shadin <shadin710@gmail.com>
2020-04-15 21:24:02 +0600

    Added insertion

--- controller/insert.js
+++ controller/insert.js
@@ -2,11 +2,46 @@ const bodyParser = require('body-parser');
 const express =  require('express');
 const router =  require('express').Router();
 const path = require('path');
+const {check,validationResult} =  require('express-validator');
 const curdUser = require('./../models/UserSchema');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
 router.use(express.static(path.join(__dirname+'./../views')));
+
+router.post('/',
+    [
+        check('username').not().isEmpty().trim().escape(),
+        check('age').not().isEmpty().trim().escape()
+    ],
+    (req,res)=>{
+        const error =  validationResult(req);
+        if(!error.isEmpty()){
+            return res.json({
+                status: false,
+                message: 'validation error',
+                error: error.array()
+            });
+        }
+
+        curdUser.create(
+            {
+                username: req.body.username,
+                age:req.body.age
+            },
+            (error,result)=>{
+                if(error){
+                    return res.json({
+                        status:false,
+                        message:'Insertion failed',
+                        error: error.array()
+                    });
+                }
+
+                res.send('well done the data has been inserted')
+            }
+        )
+    })
 
 module.exports= router;
