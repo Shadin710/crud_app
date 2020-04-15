5b8ee471f0c8ade4fe56eee4d525feae8c38cd99
Shadin <shadin710@gmail.com>
2020-04-15 21:08:01 +0600

    Added Routes

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const bodyParser = require('body-parser');
 const cors =  require('cors');
 const path = require('path');
 const database =  require('./database');
+const userController = require('./controller/insert');
 
 app.use(express.static(path.join(__dirname,'views')));
 
 app.set('view engine','pug');
 app.set ('views','./views');
 
+app.use(morgan('dev'));
+app.use(cors());
+
 app.get('/',(req,res)=>{
     res.render('index');
-})
+});
+app.use('/insert',userController);
+app.use('/find',userController);
+app.use('/update',userController);
+app.use('/delete',userController);
 
 app.listen(port,()=>{
     console.log(`Listening to ${port}.....`);
