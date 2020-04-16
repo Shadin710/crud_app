a440a42086341130fa0f7f174056034da0e5369d
Shadin <shadin710@gmail.com>
2020-04-16 21:33:26 +0600

    Added the reading method

--- controller/find.js
+++ controller/find.js
@@ -3,10 +3,42 @@ const express =  require('express');
 const router =  require('express').Router();
 const path = require('path');
 const curdUser = require('./../models/UserSchema');
+const {check,validationResult} =  require('express-validator');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
 router.use(express.static(path.join(__dirname+'./../views')));
 
+router.get('/',
+    [
+        check('username').not().isEmpty().trim().escape()
+    ],
+    (req,res)=>{
+        const error = validationResult(req);
+        if(!error.isEmpty())
+        {
+            return res.json({
+                status: false,
+                message: 'validation error',
+                error: error.array()
+            });
+        }
+
+        //no validation error
+        curdUser.findOne({username: req.query.username},(error,result)=>{
+            if(error)
+            {
+                return res.json({
+                    status: false,
+                    message: 'user not found',
+                    error: error.array()
+                });
+            }
+
+            //user found
+            res.send('user found at last');
+        })
+
+    });
 module.exports= router;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors =  require('cors');
 const path = require('path');
 const database =  require('./database');
 const userController = require('./controller/insert');
-
+const userFind =  require('./controller/find');
 app.use(express.static(path.join(__dirname,'views')));
 
 app.set('view engine','pug');
@@ -21,7 +21,7 @@ app.get('/',(req,res)=>{
     res.render('index');
 });
 app.use('/insert',userController);
-app.use('/find',userController);
+app.use('/find',userFind);
 app.use('/update',userController);
 app.use('/delete',userController);
 
